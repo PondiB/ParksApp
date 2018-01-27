e8d95c9f9584ff5a2424b8f649e8bd40279d8dd3
Pondi Brian <pondibrian@gmail.com>
2018-01-27 22:50:18 +0300

    New Project

--- app/src/androidTest/java/com/example/pondibrian/nairobiparks/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/pondibrian/nairobiparks/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.pondibrian.parks;
+package com.example.pondibrian.nairobiparks;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.pondibrian.parks", appContext.getPackageName());
+        assertEquals("com.example.pondibrian.nairobiparks", appContext.getPackageName());
     }
 }
