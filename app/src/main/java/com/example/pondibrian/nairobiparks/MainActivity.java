e9b5a5edca77348d0c9ea61fea883c888a6bbb50
Pondi Brian <pondibrian@gmail.com>
2018-01-29 15:31:03 +0300

    Main menu to be resolved

--- app/src/main/java/com/example/pondibrian/nairobiparks/MainActivity.java
+++ app/src/main/java/com/example/pondibrian/nairobiparks/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.pondibrian.nairobiparks;
 
+import android.content.res.Configuration;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,20 +28,56 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onDrawerOpened(View drawerView) {
                 super.onDrawerOpened(drawerView);
-                if(getSupportActionBar() != null)
+                if(getSupportActionBar() != null) {
                     getSupportActionBar().setTitle(R.string.drawer_opened);
+                }
             }
 
             @Override
             public void onDrawerClosed(View drawerView) {
                 super.onDrawerClosed(drawerView);
-                if(getSupportActionBar() != null)
+                if(getSupportActionBar() != null) {
                     getSupportActionBar().setTitle(R.string.drawer_closed);
+                }
 
             }
         };
+
         mDrawerLayout.addDrawerListener( mActionBarDrawerToggle);
     }
 
+    @Override
+    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onPostCreate(savedInstanceState, persistentState);
+        mActionBarDrawerToggle.syncState();
+    }
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mActionBarDrawerToggle.onConfigurationChanged(newConfig );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+  //      return super.onOptionsItemSelected(item);
+        if(mActionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+  //      int id = item.getItemId();
+
+  //     if (id == R.id.action_settings){
+ //          return true;
+  //      }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
